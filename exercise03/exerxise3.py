e9922a50e17ae529d8808c3472533a4f2d383783
kodaig <kodaig06@gmail.com>
2017-05-11 11:50:52 +0200

    Have a look at the comment below
    I set up constraint Matrix A and coefficients vector c with regard to exercise3.
    Basically it worked well, however has a few issues.
    1. 
    For each β value,  there are 2 lowest energy states. (calculate them by hand)
    i.e.) when β=1.0, lowest energy states are (x0,x1,x2)=(0,0,0) and (1,1,1) with their energy 1.1
           as well when β=-1.0, the lowest energy states are (0,0,1) and (1,0,1) with their energy -1.7
    However, scipy.optimize.linsprog only gives one solution vector for each situation. In fact, I got only
    (0,0,0) for β=1.0
    2.
    Since scipy.optimize.linsprog is LP solver not ILP solver, it usually returns an non-integer vector.
    If we want to make ILP solver, we need further work?

--- exercise03/exerxise3.py
+++ exercise03/exerxise3.py
@@ -1,1 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import linprog
 
+beta = 1.0
+
+#coefficient vector
+c = np.array([0.1,0.1,0.1,0.9,0.9,0.1,0,beta,beta,0,0,beta,beta,0,0,beta,beta,0])
+"""
+I set solution vector "mu" as
+mu=
+[mu0(0),mu0(1),mu1(0),mu1(1),mu2(0),mu2(1)
+,mu01(0,0),mu01(0,1),mu01(1,0),mu01(1,1)
+,mu12(0,0),mu12(0,1),mu12(1,0),mu12(1,1)
+,mu20(0,0),mu20(0,1),mu20(1,0),mu20(1,1)]
+mui(k) = 1 (xi=k), 0 (otherwise)
+muij(k,l) = 1 ((xi,xj)=(k,l)), 0 (otherwise)
+"""
+
+#constraint matrix
+A = np.array([[0]*18]*15)
+for i in range(3):
+    A[i,2*i] = 1
+    A[i,2*i+1] = 1
+
+for i in range(3,15):
+    j = i - 3
+    k = j - 2*(j//4)
+    A[i,k-6*(k//6)] = -1    
+
+a = np.array([[1,1,0,0],[0,0,1,1],[1,0,1,0],[0,1,0,1]])
+A[3:7,6:10] = a
+A[7:11,10:14] = a
+A[11:15,14:18] = a
+
+b = np.array([0]*15)
+for i in range(3):
+    b[i] = 1
+
+bounds = (0, None)
+
+res = linprog(c, A_eq=A, b_eq=b, bounds=(bounds), options={"disp": True})
+
+print("beta=",beta)
+print("coefficients vector c=\n",c)
+print("constraint matrix A=\n",A)
+print("solution vector mu=\n",res.x)
