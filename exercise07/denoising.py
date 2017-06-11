6dde1a9985ffdd000bbebf3ca0488dc2eb7bcb68
Yuyan Li <yuyan.li@gmx.net>
2017-06-12 01:54:34 +0200

    add image saving

--- exercise07/denoising.py
+++ exercise07/denoising.py
@@ -7,7 +7,7 @@ import time
 
 def norm01(data):
     res = data.astype('float')
-    # res -= res.min()
+    res -= res.min()
     res /= res.max()
     return res
 
@@ -22,6 +22,7 @@ def buildConstQ(imshape, beta = -1.):
 
     diagElem = 4
 
+    # vectorized index
     def getInd(x, y):
         return x + y * ncols
 
@@ -52,9 +53,11 @@ def buildFancyQ(im, gamma, alpha=-1.):
     
     Q = scipy.sparse.lil_matrix((size,size), dtype='float')
 
+    # off diagonal elements
     def getBeta(c0, c1):
         return alpha * np.exp(-gamma * np.linalg.norm(c0 - c1))
 
+    # vectorized index
     def getInd(x, y):
         return x + y * ncols
 
@@ -65,27 +68,29 @@ def buildFancyQ(im, gamma, alpha=-1.):
             i = getInd(x,y)
 
             if x < ncols-1:
-                beta = getBeta(c0, im[x+1, y])
                 j = getInd(x+1, y)
+
+                beta = getBeta(c0, im[x+1, y])
                 Q[i,j] = beta
                 Q[j,i] = beta
+
                 Q[i,i] += abs(beta)
                 Q[j,j] += abs(beta)
 
             if y < nrows-1:
-                beta = getBeta(c0, im[x, y+1])
                 j = getInd(x, y+1)
+
+                beta = getBeta(c0, im[x, y+1])
                 Q[i,j] = beta
                 Q[j,i] = beta
+
                 Q[i,i] += abs(beta)
                 Q[j,j] += abs(beta)
         
-    # for i in range(size):
-    #     Q[i,i] = abs(np.sum(Q[i]))
-    
     print("Fancy Q built!")
 
     return Q.tocsc()
+
 
 def denoise(img, Q, sigma = 1.0):
     shape = img.shape
@@ -104,17 +109,19 @@ def denoise(img, Q, sigma = 1.0):
 
         result[:,:,c] = z.reshape(shape[0:2])
         
-        print("Solved for color ", c)
+        print("Solved for color", c)
 
     return result
 
 
 # parameters
-noise = 0.1
+noise = 0.15
 sigma = 1.0
-gamma = 3.
+gamma = 2.
 
+# time it
 start = time.time()
+
 # load image
 gt_img = ski.data.astronaut()
 gt_img = norm01(gt_img)
@@ -124,8 +131,12 @@ shape = gt_img.shape
 size = shape[0] * shape[1]
 
 # add noise
-noisy_img = ski.util.random_noise(gt_img, var=noise**2)
-noisy_img = np.clip(noisy_img, 0, 1)
+# noisy_img = ski.util.random_noise(gt_img, var=noise**2)
+# noisy_img = np.clip(noisy_img, 0, 1)
+
+# load noisy image
+noisy_img = ski.io.imread("noisy.png")
+noisy_img = norm01(noisy_img)
 
 
 # constQ = buildConstQ(shape[0:2], -.5)
@@ -136,20 +147,26 @@ fancyQ = buildFancyQ(noisy_img, gamma)
 result_img = denoise(noisy_img, fancyQ, sigma)
 result_img = norm01(result_img)
 
-print("Time from loading image to denoised image: ", time.time() - start)
+print("Time from loading image to denoised image:", time.time() - start)
 
 
-plt.subplot(131)
-plt.imshow(gt_img)
-plt.title("Original")
+plt.imsave("denoised.png", result_img)
 
-plt.subplot(132)
+plt.figure(figsize=(10, 6))
+# plt.subplot(221)
+# plt.imshow(gt_img)
+# plt.title("Original")
+# plt.axis('off')
+
+plt.subplot(121)
 plt.imshow(noisy_img)
 plt.title("Noisy")
+plt.axis('off')
 
-plt.subplot(133)
+plt.subplot(122)
 plt.imshow(result_img)
 plt.title("Denoised")
+plt.axis('off')
 
 plt.suptitle(performance(gt_img, result_img))
 
