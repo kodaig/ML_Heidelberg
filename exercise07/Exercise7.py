e39a917f60795adbecbd2ac4059f1bab63c7f705
Yuyan Li <yuyan.li@gmx.net>
2017-06-12 11:53:43 +0200

    add some white space

--- exercise07/Exercise7.py
+++ exercise07/Exercise7.py
@@ -4,6 +4,7 @@ import skimage
 import skimage.data
 import matplotlib.pyplot as plt
 from scipy.sparse import csr_matrix, lil_matrix
+import time
 
 def makeQ_simple(im):
     m,n,e = im.shape
@@ -24,26 +25,29 @@ def makeQ_simple(im):
     return Q.tocsr()
 
 def makeQ_fancy(im,gamma,alpha=-1):
+    start = time.time()
     #print("alpha:",alpha,"gamma:",gamma)
     m,n,ch = im.shape
-    L=m*n
-    im=im.reshape((L,3))
-    Q=lil_matrix((L,L))
+    L = m*n
+    im = im.reshape((L,3))
+    Q = lil_matrix((L,L))
     
     for i in range(L-1):
-        diff=np.linalg.norm(im[i+1]-im[i])
-        C=alpha*np.exp(-gamma*diff)
-        Q[i,i+1]=C
-        Q[i+1,i]=C
+        diff = np.linalg.norm(im[i+1]-im[i])
+        C = alpha*np.exp(-gamma*diff)
+        Q[i,i+1] = C
+        Q[i+1,i] = C
     
     for i in range(L-n):
-        diff=np.linalg.norm(im[i+n]-im[i])
-        C=alpha*np.exp(-gamma*diff)
-        Q[i,i+n]=C
-        Q[i+n,i]=C
+        diff = np.linalg.norm(im[i+n]-im[i])
+        C = alpha*np.exp(-gamma*diff)
+        Q[i,i+n] = C
+        Q[i+n,i] = C
         
     for i in range(L):
-        Q[i,i]=abs(np.sum(Q[i]))
+        Q[i,i] = abs(np.sum(Q[i]))
+
+    print("Fancy Q built in", time.time()-start)
     
     return Q.tocsr()
 
@@ -59,16 +63,16 @@ L = height*width
 #Denoising with simple Q
 sigma = 0.7
 
-A = sigma**2*makeQ_simple(pic) + scipy.sparse.identity(L,format="csr")
+A = sigma**2 * makeQ_simple(pic) + scipy.sparse.identity(L,format="csr")
 
-pic_denoised=[]
+pic_denoised = []
 for i in range(3):
-    b=pic_noisy[:,:,i].reshape((L,))
-    x = scipy.sparse.linalg.spsolve(A,b)
+    b = pic_noisy[:,:,i].reshape((L,))
+    x  =  scipy.sparse.linalg.spsolve(A,b)
     pic_denoised.append(x.reshape((L,1)))
     
-pic_denoised=np.concatenate(pic_denoised,axis=1)
-pic_denoised=pic_denoised.reshape((height,width,3))
+pic_denoised = np.concatenate(pic_denoised,axis=1)
+pic_denoised = pic_denoised.reshape((height,width,3))
 
 print("noisy","Sum Squared Defference=",np.linalg.norm(pic-pic_noisy)/L)
 plt.imshow(pic_noisy, interpolation="nearest")
@@ -79,15 +83,15 @@ plt.imshow(pic_denoised, interpolation="nearest")
 plt.show()
 
 #Denoising with fancy Q
-sigma=1.1
-alpha=-1
-gamma=3
+sigma = 1.1
+alpha = -1
+gamma = 3
 
 A = sigma**2*makeQ_fancy(pic,gamma) + scipy.sparse.identity(L,format="csr")
 
-pic_denoised=[]
+pic_denoised = []
 for i in range(3):
-    b=pic_noisy[:,:,i].reshape((L,))
+    b = pic_noisy[:,:,i].reshape((L,))
     x = scipy.sparse.linalg.spsolve(A,b)
     pic_denoised.append(x.reshape((L,1)))
     
